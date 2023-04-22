5037bec1b88956452b3f5635d46a32e17c537a25
Odds <panerjeffryjames@gmail.com>
2023-04-22 11:27:48 +0800

    add pause and resume button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let timeDisplay = document.getElementById("time");
 let startBtn = document.getElementById("start");
+let resumeBtn = document.getElementById("resume");
+let pauseBtn = document.getElementById("pause");
+let slider = document.getElementById("slider")
 let sliderDuration = document.getElementById("duration");
 let sliderValue = document.getElementById("slider-value")
 let timerElement = document.getElementById("timer")
@@ -7,29 +10,52 @@ let clockElement = document.getElementById("clock");
 sliderValue.innerHTML = sliderDuration.value;
 timerElement.innerHTML = sliderDuration.value + " mins";
 
-function displayCurrentTime() {
+setInterval(() => {
     const now = new Date();
-
-    // Format the time as HH:MM:SS
     const hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
     const time = `${hours}:${minutes}`;
-
-    // Update the clock display
     clockElement.textContent = time;
-}
-setInterval(displayCurrentTime, 1000)
+}, 0)
 
 
+let paused = false;
 let timer;
+let remainingTime;
 
 function startTimer() {
+    if (!paused) {
+        clearInterval(timer)
+        const duration = parseInt(sliderDuration.value) * 60 * 1000; //convert minutes t0 milliseconds
+        remainingTime = duration;
+        timer = setInterval(() => {
+            const minutes = Math.floor((remainingTime / 1000 / 60) % 60);
+            const seconds = Math.floor((remainingTime / 1000) % 60);
+            timerElement.innerHTML = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+            remainingTime -= 1000;
+
+            if (remainingTime < 0) {
+                clearInterval(timer);
+                timerElement.innerHTML = "0:00";
+                timerElement.innerHTML = "Break time!";
+
+            }
+        }, 1000)
+    }
+}
+
+function breakTime() {
+
+}
+function pauseInterval() {
     clearInterval(timer)
-    const duration = parseInt(sliderDuration.value) * 60 * 1000; //convert minutes t0 milliseconds
-    let remainingTime = duration;
+    paused = true;
 
+}
+
+function resumeInterval() {
+    paused = false;
     timer = setInterval(() => {
-
         const minutes = Math.floor((remainingTime / 1000 / 60) % 60);
         const seconds = Math.floor((remainingTime / 1000) % 60);
         timerElement.innerHTML = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
@@ -39,15 +65,39 @@ function startTimer() {
             clearInterval(timer);
             timerElement.innerHTML = "0:00";
         }
-
-    }, 1000)
-
+    }, 1000);
 }
-startBtn.addEventListener("click", startTimer);
 
+startBtn.addEventListener("click", function () {
+    startTimer();
+    slider.style.display = "none";
+    startBtn.style.display = "none"
+    pauseBtn.style.display = "block";
+});
+resumeBtn.addEventListener("click", function () {
+    resumeInterval();
+    slider.style.display = "none";
+    resumeBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    timerElement.classList.remove("text-gray-400")
+});
+pauseBtn.addEventListener("click", function () {
+    pauseInterval();
+    resumeBtn.style.display = "block";
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "none";
+    timerElement.classList.add("text-gray-400");
+    slider.style.display = "flex";
+
+});
 
 sliderDuration.oninput = function () {
+    clearInterval(timer);
+    paused = false;
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "none";
+    timerElement.classList.remove("text-gray-400");
     sliderValue.innerHTML = this.value;
     timerElement.innerHTML = this.value + " mins";
-    clearInterval(timer)
 }
