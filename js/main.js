6e584e566f5c91df97a64f7bcc1c6b2a126e959a
Odds <panerjeffryjames@gmail.com>
2023-04-25 13:07:40 +0800

    comit

--- js/main.js
+++ js/main.js
@@ -57,31 +57,41 @@ const emptyTime = () => {
         title.innerHTML = `<span class="text-blue-600">Congratulations!</span> <br> You're one task ahead.`;
         timeEstimate.innerHTML = "Begin new task.";
         minElement.style.display = "none";
-        // canvas.style.display = "flex";
-        // initConfetti();
-        // render();
-        var duration = 15 * 1000;
-        var animationEnd = Date.now() + duration;
-        var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-        function randomInRange(min, max) {
-            return Math.random() * (max - min) + min;
+        var count = 250;
+        var defaults = {
+            origin: { y: 0.8 }
+        };
+
+        function fire(particleRatio, opts) {
+            confetti(Object.assign({}, defaults, opts, {
+                particleCount: Math.floor(count * particleRatio)
+            }));
         }
 
-        var interval = setInterval(function () {
-            var timeLeft = animationEnd - Date.now();
+        fire(0.25, {
+            spread: 26,
+            startVelocity: 55,
+        });
+        fire(0.2, {
+            spread: 60,
+        });
+        fire(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8
+        });
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2
+        });
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 45,
+        });
 
-            if (timeLeft <= 0) {
-                return clearInterval(interval);
-            }
-
-            var particleCount = 50 * (timeLeft / duration);
-            // since particles fall down, start a bit higher than random
-            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
-        }, 250);
-
-        
     } else if (remainingTime < 59000) {
         minElement.innerHTML = "sec";
     } else if (remainingTime == 59000) {
